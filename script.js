5e7b09f62ded16126e7a2399d1df0daaf4de6136
karthikeyan <karthikeyanvcet17@gmail.com>
2020-06-20 16:08:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var x=Math.floor(Math.random()*10 + 1);
+var guess=1;
+console.log(x);
+document.getElementById("submit").onclick=function(){
+    var y=document.getElementById("input").value;
+    if(x==y){
+        alert("CONGRADULATIONS!!! You Guessed it right in "+guess+" Guess");
+        location.reload();
+    }
+    else if(x<y){
+        guess++;
+        alert("Try a smaller Number");
+    }
+    else{
+        guess++;
+        alert("Try a larger Number");
+    }
+}
